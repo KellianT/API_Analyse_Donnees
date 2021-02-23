bb3ac992827bf1ce677067065ac56115691dadd4
KellianT <kellianth@outlook.com>
2021-02-23 16:56:57 +0100

    Fonction n°2 local ok / server pas ok

--- views.py
+++ views.py
@@ -3,13 +3,13 @@ import logging
 from flask import Flask, abort
 app = Flask(__name__)
 import csv
-import pandas
+import pandas as pd
 import unittest
 
 logging.basicConfig(level=logging.DEBUG)
 
 #url = 'https://data.un.org/_Docs/SYB/CSV/SYB63_310_202009_Carbon%20Dioxide%20Emission%20Estimates.csv'
-board = pandas.read_csv('onu.csv', header = 1, usecols=['Region/Country/Area', 'Unnamed: 1', 'Value', 'Year', 'Series'])
+board = pd.read_csv('onu.csv', header = 1, usecols=['Region/Country/Area', 'Unnamed: 1', 'Value', 'Year', 'Series'])
 db_onu = board.rename(columns={'Region/Country/Area': 'num', 'Unnamed: 1': 'Country'})
 #print(db_onu.head(1))
 
@@ -39,8 +39,29 @@ def by_country(country):
         print("erreur 404")
     # #     #erreur 404 si on demande un pays qui n'est pas connu
     #     abort(404)
-print(hello_world())
-print(by_country('ALBAnia'))
+# print(hello_world())
+# print(by_country('ALBAnia'))
 
-# if __name__ == "__main__":
-#     app.run()
+@app.route('/average_by_year/<year>')
+def average_for_year(year):
+    
+    
+    # file = open("onu.csv","r")
+    # test=csv.reader(file)
+    # for row in test:
+    #     print(row[1])
+
+    #on cherche la moyenne des émissions totales au niveau mondial pour une année demandée
+    # logging.debug(f"Année demandée : {year}")
+    all_years = list(set(db_onu['Year'].to_list()))
+    #print(all_years)
+    if year in all_years:
+        df = db_onu[(db_onu['Year'] == year) & (db_onu['Series'] == 'Emissions (thousand metric tons of carbon dioxide)')]
+        mean_df= df.mean().round()['Value']
+        return json.dumps({"year":"1975", "total":mean_df})
+    else:
+        #abort(404)
+        print("c'est la zone")
+#print(average_for_year(2015))
+if __name__ == "__main__":
+    app.run()
