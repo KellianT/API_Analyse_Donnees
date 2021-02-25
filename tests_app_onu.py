df0f977ee799056396e18e37dbe015cbec56de77
emilie-lb <emilielb75@gmail.com>
2021-02-25 19:24:21 +0100

    ajout fichier test_app_onu et tests sur app_onu, class TestAppFlask et class TestAverageByYear

---
+++ tests_app_onu.py
@@ -0,0 +1,59 @@
+import unittest
+from flask import Flask
+from app_onu import app, hello_world, average_for_year
+
+
+class TestAppFlask(unittest.TestCase):
+
+    def test_home_status_code(self):
+        # sends HTTP GET request to the application
+        # on the specified path
+        self.app = app.test_client()
+        result = self.app.get('/')
+        # assert the status code of the response
+        self.assertEqual(result.status_code, 200) 
+
+    def test_hello_world(self):
+        # sends HTTP GET request to the application
+        # on the specified path
+        self.app = app.test_client()
+        result = self.app.get('/') 
+        # assert the response data
+        self.assertEqual(result.data, b'Hello, World') # voir pourquoi il y a b avant la sortie...???
+
+
+
+class TestAverageByYear(unittest.TestCase):
+
+    def test_average_by_year_status(self):
+        # sends HTTP GET request to the application
+        # on the specified path
+        self.app = app.test_client()
+        result = self.app.get("/average_by_year/2015")
+        # assert the status code of the response
+        self.assertEqual(result.status_code, 200) 
+
+    def test_average_by_year_type(self):
+        # sends HTTP GET request to the application
+        # on the specified path
+        self.app = app.test_client()
+        result = self.app.get("/average_by_year/2015")
+        # assert the type of the response
+        self.assertEqual(result.content_type, "application/json")
+
+    def test_average_by_year_data(self):
+        self.app = app.test_client()
+        result = self.app.get("/average_by_year/2015")
+        # self.assertTrue(b'Total' in response.data)
+        self.assertTrue(b'year' in result.data)
+
+#test complémetnaire sur une valeur précise (pas forcement pertinent)
+    def test_average_by_year_specific_data(self):
+        self.app = app.test_client()
+        result = self.app.get("/average_by_year/2015")
+        # self.assertTrue(b'Total' in response.data)
+        self.assertEqual(result.data, b'{"year":"2015","total":217617.02909154928}\n')
+
+
+if __name__ == '__main__':
+    unittest.main()
