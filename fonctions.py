1770dfc95b1439ee7ca10bf16c80026f5de320b3
emilie-lb <emilielb75@gmail.com>
2021-02-25 21:07:24 +0100

    ajout log.info et log.warning dans les fichiers app_onu.py et fonctions.py

--- fonctions.py
+++ fonctions.py
@@ -6,11 +6,13 @@ import unittest
 
 
 def voir_csv(csv):
+    logging.info("création de la base de données")
     db_onu = pandas.read_csv(csv, header = 2, names = [ 'num','Country', 'Year','Emission','Value','Footnote','Source'])
     return db_onu
 
 
 def country_inall_countries(country,db_onu):
+    logging.info("vérification si le pays demandé existe")
     country = country.title()
     countries = list(set(db_onu['Country'].to_list()))
     if country in countries:
@@ -19,6 +21,7 @@ def country_inall_countries(country,db_onu):
         return False
 
 def year_inall_years(year, db_onu):
+    logging.info("vérification si l'année demandée existe")
     all_years = list(set(db_onu['Year'].to_list()))
     if int(year) in all_years: 
         return True
@@ -27,6 +30,7 @@ def year_inall_years(year, db_onu):
 
 
 def by_country_to_json(country,db_onu):
+    logging.info("création du dictionnaire dans fonction by_country_to_json")
     country_emission = db_onu[(db_onu["Country"] == country) & (db_onu["Year"]==2017)].head(1)
     result = {}
     result['Country'] = str(country_emission.iloc[0][1])
@@ -37,6 +41,7 @@ def by_country_to_json(country,db_onu):
 
 
 def mean_to_json(year, db_onu): 
+    logging.info("création du dictionnaire dans fonction mean_to_json")
     df = db_onu.loc[db_onu['Year'].isin([year])]
     df = df[(df["Emission"]=='Emissions (thousand metric tons of carbon dioxide)')]
     df_mean = df.mean()['Value']
@@ -46,6 +51,7 @@ def mean_to_json(year, db_onu):
     return result
 
 def per_capita_to_json(country, db_onu):
+    logging.info("création du dictionnaire dans fonction per_capita_to_json")
     df_capita = db_onu[(db_onu["Country"] == country) & (db_onu["Emission"]=="Emissions per capita (metric tons of carbon dioxide)")]  
     df_capita = df_capita[['Year', 'Value']]
     result = {}
