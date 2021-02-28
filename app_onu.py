b3ac5a7338e6cb53887871f2c4a50d27a0706d24
emilie-lb <emilielb75@gmail.com>
2021-02-28 20:58:34 +0100

    Ajout des docstrings

--- app_onu.py
+++ app_onu.py
@@ -1,13 +1,9 @@
 import fonctions as fct
 import logging
-from flask import Flask, abort, jsonify, render_template, request, url_for, flash, redirect
+from flask import Flask, abort, jsonify, render_template,\
+    request, url_for, flash, redirect
 app = Flask(__name__)
 app.config['JSON_SORT_KEYS'] = False
-
-# import csv
-# import json
-# import pandas
-# import unittest
 
 # logging.basicConfig(level=logging.DEBUG)
 logging.basicConfig(
@@ -17,18 +13,27 @@ logging.basicConfig(
     datefmt='%d/%m/%Y %H:%M:%S',)
 
 
-
-
 @app.route('/')
 def hello_world():
-    """
-    """
+    """ This function says 'Hello, World'. """
     # utilisé pour tester si l'app fonctionne bien
     return 'Hello, World'
 
 
 @app.route('/latest_by_country/<country>')
 def by_country(country):
+    """ This function searches for country emissions
+    (thousand metric tons of carbon dioxide) latest value.
+    Parameter : a country (ex: France)
+    Returns : json data
+    Example :
+    for France (127.0.0.1:5000/latest_by_country/France)
+    {
+    "Country": "France",
+    "Year": 2017,
+    "Value": 306123.541
+    }
+    """
     # valeur la plus récente des emissions totales pour le pays demandé
     logging.info(f"Pays demandé : {country}")
     db_onu = fct.voir_csv("onu.csv")
@@ -43,6 +48,17 @@ def by_country(country):
 
 @app.route('/average_by_year/<year>')
 def average_for_year(year):
+    """ This function searches for average emissions
+    (thousand metric tons of carbon dioxide) value for a set year.
+    Parameter : a year (ex: 2015)
+    Returns : json data
+    Example :
+    for 2015 (http://127.0.0.1:5000/average_by_year/2017)
+    {
+    "year": "2015",
+    "total": 217617.02909154928
+    }
+    """
     # moyenne des émissions mondiales totales pour une année demandée
     logging.info(f"Année demandée : {year}")
     db_onu = fct.voir_csv("onu.csv")
@@ -56,6 +72,23 @@ def average_for_year(year):
 
 @app.route('/per_capita/<country>')
 def per_capita(country):
+    """ This function shows for country emissions
+    per capita (metric tons of carbon dioxide) values.
+    Parameter : a country (ex: Italy)
+    Returns : json data
+    Example :
+    for Italy (http://127.0.0.1:5000/per_capita/ITALY)
+    {
+    "1975": 5.719,
+    "1985": 6.044,
+    "1995": 7.056,
+    "2005": 7.844,
+    "2010": 6.552,
+    "2015": 5.428,
+    "2016": 5.371,
+    "2017": 5.31
+    }
+    """
     # toutes les emissions per capita pour un pays donné
     logging.info(f"Pays demandé : {country}")
     db_onu = fct.voir_csv("onu.csv")
--- fonctions.py
+++ fonctions.py
@@ -1,8 +1,5 @@
 import logging
 import pandas
-# import json
-# import csv
-# import unittest
 
 
 def voir_csv(csv):
@@ -12,6 +9,12 @@ def voir_csv(csv):
 
 
 def country_inall_countries(country, db_onu):
+    """ This function checks whether the set country
+    is included into the database (db_onu).
+    Parameter : a country (ex: Albania), a database
+    Returns : True or Flase
+    Example : Albania returns True, Dysneyland returns False
+    """
     logging.info("vérification si le pays demandé existe")
     country = country.title()
     countries = list(set(db_onu['Country'].to_list()))
@@ -22,6 +25,12 @@ def country_inall_countries(country, db_onu):
 
 
 def year_inall_years(year, db_onu):
+    """ This function checks whether the set year
+    is included into the database (db_onu).
+    Parameter : a year (ex: 2015), a database
+    Returns : True or Flase
+    Example : 2015 returns true, 1974 returns false
+    """
     logging.info("vérification si l'année demandée existe")
     all_years = list(set(db_onu['Year'].to_list()))
     if int(year) in all_years:
@@ -31,6 +40,13 @@ def year_inall_years(year, db_onu):
 
 
 def by_country_to_json(country, db_onu):
+    """ This function searches for country emissions
+    (thousand metric tons of carbon dioxide) latest value.
+    Parameter : a country (ex: France), a database
+    Returns : a dictionnary
+    Example : for France returns
+    {"Country": "France", "Year": 2017, "Value": 306123.541}
+    """
     logging.info("création du dictionnaire dans fonction by_country_to_json")
     country_emission = db_onu[(db_onu["Country"] == country) & (db_onu["Year"] == 2017)].head(1)
     result = {}
@@ -41,6 +57,13 @@ def by_country_to_json(country, db_onu):
 
 
 def mean_to_json(year, db_onu):
+    """ searches for average emissions
+    (thousand metric tons of carbon dioxide) value for a set year.
+    Parameter : a year (ex: 2015)
+    Returns : a dictionnary
+    Example : for 2015
+    {"year": "2015", "total": 217617.02909154928}
+    """
     logging.info("création du dictionnaire dans fonction mean_to_json")
     df = db_onu.loc[db_onu['Year'].isin([year])]
     df = df[(df["Emission"] == 'Emissions (thousand metric tons of carbon dioxide)')]
@@ -52,6 +75,14 @@ def mean_to_json(year, db_onu):
 
 
 def per_capita_to_json(country, db_onu):
+    """ This function shows for country emissions
+    per capita (metric tons of carbon dioxide) values.
+    Parameter : a country (ex: Italy)
+    Returns : a dictionnary
+    Example : for Italy returns
+    { "1975": 5.719, "1985": 6.044, "1995": 7.056, "2005": 7.844,
+    "2010": 6.552, "2015": 5.428, "2016": 5.371, "2017": 5.31}
+    """
     logging.info("création du dictionnaire dans fonction per_capita_to_json")
     df_capita = db_onu[(db_onu["Country"] == country) & (db_onu["Emission"] == "Emissions per capita (metric tons of carbon dioxide)")]
     df_capita = df_capita[['Year', 'Value']]
