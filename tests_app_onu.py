94061497807e11382c123d7b82e634f8b0c7651b
emilie-lb <62951146+emilie-lb@users.noreply.github.com>
2021-03-01 14:27:31 +0100

    Update tests_app_onu.py

--- tests_app_onu.py
+++ tests_app_onu.py
@@ -8,7 +8,7 @@ class TestAppFlask(unittest.TestCase):
     def test_home_status_code(self):
         ''' Function that allows us to retrieve the response code of the query
             sends HTTP GET request to the application
-            on the specified path '''
+            on the specified path'''
         self.app = app.test_client()
         result = self.app.get('/')
         self.assertEqual(result.status_code, 200)
