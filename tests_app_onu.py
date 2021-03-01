bd123b9c63f1cd88e842206ab5cd2f0ebe878316
Y4N1S <yanisboulot@gmail.com>
2021-03-01 10:49:07 +0100

    Ajout docstring

--- tests_app_onu.py
+++ tests_app_onu.py
@@ -6,41 +6,41 @@ from app_onu import app
 class TestAppFlask(unittest.TestCase):
 
     def test_home_status_code(self):
-        # sends HTTP GET request to the application
-        # on the specified path
+        ''' Function that allows us to retrieve the response code of the query
+            sends HTTP GET request to the application 
+            on the specified path '''
         self.app = app.test_client()
         result = self.app.get('/')
-        # assert the status code of the response
         self.assertEqual(result.status_code, 200)
 
     def test_hello_world(self):
-        # sends HTTP GET request to the application
-        # on the specified path
+        ''' Fonction basic test
+            sends HTTP GET request to the application
+            on the specified path '''
         self.app = app.test_client()
         result = self.app.get('/')
-        # assert the response data
         self.assertEqual(result.data, b'Hello, World')
 
 
 class TestLatestByCountry(unittest.TestCase):
 
     def test_latest_by_country_status(self):
-        # sends HTTP GET request to the application
-        # on the specified path
+        ''' Function that allows us to retrieve the response code of the query
+            sends HTTP GET request to the application
+            on the specified path '''
         self.app = app.test_client()
         result = self.app.get("/latest_by_country/France")
-        # assert the status code of the response
         self.assertEqual(result.status_code, 200)
 
     def test_latest_by_country_type(self):
-        # sends HTTP GET request to the application
-        # on the specified path
+        ''' sends HTTP GET request to the application
+            on the specified path '''
         self.app = app.test_client()
         result = self.app.get("/latest_by_country/France")
-        # assert the type of the response
         self.assertEqual(result.content_type, "application/json")
 
     def test_latest_by_country_data(self):
+        ''' verification of the data in the results ''' 
         self.app = app.test_client()
         result = self.app.get("/latest_by_country/France")
         self.assertTrue(b'Country' in result.data)
@@ -49,22 +49,23 @@ class TestLatestByCountry(unittest.TestCase):
 class TestAverageByYear(unittest.TestCase):
 
     def test_average_by_year_status(self):
-        # sends HTTP GET request to the application
-        # on the specified path
+        ''' Function that allows us to retrieve the response code of the query
+            sends HTTP GET request to the application
+            on the specified path '''
         self.app = app.test_client()
         result = self.app.get("/average_by_year/2015")
-        # assert the status code of the response
         self.assertEqual(result.status_code, 200)
 
     def test_average_by_year_type(self):
-        # sends HTTP GET request to the application
-        # on the specified path
+        ''' verifies the type of data
+            sends HTTP GET request to the application
+            on the specified path '''
         self.app = app.test_client()
         result = self.app.get("/average_by_year/2015")
-        # assert the type of the response
         self.assertEqual(result.content_type, "application/json")
 
     def test_average_by_year_data(self):
+        ''' verifies the type of data '''
         self.app = app.test_client()
         result = self.app.get("/average_by_year/2015")
         self.assertTrue(b'year' in result.data)
@@ -73,19 +74,17 @@ class TestAverageByYear(unittest.TestCase):
 class TestPerCapits(unittest.TestCase):
 
     def test_per_capita_status(self):
-        # sends HTTP GET request to the application
-        # on the specified path
+        ''' sends HTTP GET request to the application
+            on the specified path '''
         self.app = app.test_client()
         result = self.app.get("/per_capita/France")
-        # assert the status code of the response
         self.assertEqual(result.status_code, 200)
 
     def test_per_capita_type(self):
-        # sends HTTP GET request to the application
-        # on the specified path
+        ''' sends HTTP GET request to the application
+            on the specified path '''
         self.app = app.test_client()
         result = self.app.get("/per_capita/France")
-        # assert the type of the response
         self.assertEqual(result.content_type, "application/json")
 
     def test_per_capita_data(self):
